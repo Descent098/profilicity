9d113b5baf321c3d1040484ea0dfc43095fabbc0
Kieran Wood <kieranw098@gmail.com>
2022-10-20 15:24:26 -0600

    Added more updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,27 @@
 
+user = {}
+
 function initializeEditor(){
-    usersName = document.getElementById("name").value
-    github = document.getElementById("github").value
-    instagram = document.getElementById("instagram").value
-    theme = document.getElementById("theme").value
+    // Initializes the editor
+    user["name"] = document.getElementById("name").value
+    user["github"] = document.getElementById("github").value
+    user["instagram"] = document.getElementById("instagram").value
+    user["theme"] = document.getElementById("theme").value
+    user["avatar"] = `https://avatars.dicebear.com/api/identicon/${user["name"]}.svg`
 
     // Fill Empty values
-    if (!usersName){
-        usersName = "John Doe";
+    if (!user["name"]){
+        user["name"] = "John Doe";
         document.getElementById("name").value = "John Doe"
     }
 
-    if (!github){
-        github = "https://github.com/user";
+    if (!user["github"]){
+        user["github"] = "https://github.com/user";
         document.getElementById("github").value = "https://github.com/user"
     }
 
-    if (!instagram){
-        instagram = "https://instagram.com/user";
+    if (!user["instagram"]){
+        user["instagram"] = "https://instagram.com/user";
         document.getElementById("instagram").value = "https://instagram.com/user"
     }
 
@@ -76,4 +80,21 @@ function initializeEditor(){
     // html=`<html><head><title>yeet</title></head><body><h1>Hello World!!</h1></body></html>`
     // document.getElementById("preview-pane").src = `data:text/html,${encodeURIComponent(html)}`
     document.getElementById("preview-pane").src = "https://kieranwood.ca/glass-portfolio"
+}
+
+function changeFormElement(element){
+    user[element.id] = element.value
+    // Reset avatar to use new name as seed
+    if (document.getElementById("avatar-display").src.includes("dicebear")){
+        document.getElementById("avatar-display").src=`https://avatars.dicebear.com/api/identicon/${user['name']}.svg`
+    }
+    if (element.id === "avatar"){
+        console.log("yeet")
+        reader = new FileReader();
+        reader.onload = function(e){
+            document.getElementById("avatar-display").src = e.target.result
+        }
+        reader.readAsDataURL(document.getElementById("avatar").files[0])
+    } 
+
 }
